a74597167671df0a86fe8ba535346eee1dca5759
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-02-05 00:39:52 -0300

    GameOver div created

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino')
     const grid = document.querySelector('.grid')
+    const alert= document.getElementById('alert')
     let isJumping = false
     let gravity = 0.9
+    let isGameOver = false
 
     function control(e) {
         if (e.keyCode === 32) {
@@ -17,47 +19,53 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let position = 0
     function jump() {
-        let count = 0
-        let timerId = setInterval(function () {
-
-
-            if (count === 15){
-                clearInterval(timerId)
-                console.log('down')
-                let downTimerId = setInterval(function (){
-                    if (count === 0){
-                        clearInterval(downTimerId)
-                        isJumping = false
-                    }
-                    position -= 0
-                    count --
-                    position = position * gravity
-                    dino.style.bottom = position + 'px'
-                }, 20)
-
+      let count = 0
+      let timerId = setInterval(function () {
+        //move down
+        if (count === 15) {
+          clearInterval(timerId)
+          let downTimerId = setInterval(function () {
+            if (count === 0) {
+              clearInterval(downTimerId)
+              isJumping = false
             }
-            console.log('up')
-            position += 30
+            position -= 1 
+            count--
             position = position * gravity
-            dino, style.bottom = position + 'px'
-            console.log(dino.style.bottom)
-        }, 20)
+            dino.style.bottom = position + 'px'
+          },20)
+    
+        }
+        //move up
+        position +=30
+        count++
+        position = position * gravity
+        dino.style.bottom = position + 'px'
+      },20)
     }
+    
+    function generateObstacles() {
+      let randomTime = Math.random() *4000;
+      let obstaclePosition = 850
+      const obstacle = document.createElement('div')
+      obstacle.classList.add('obstacle')
+      grid.appendChild(obstacle)
+      obstacle.style.left = obstaclePosition + 'px'
 
-    function generateObstacles(){
-        let obstaclePosition = 2000
-        const obstacle = document.createElement('div')
-        obstacle.classList.add('obstacle')
-        grid.appendChild(obstacle)
-        obstacle.style.left = obstaclePosition + 'px'
+      let timerId= setInterval(function(){
+        if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60 ){
+            clearInterval (timerId)
+            alert.innerHTML = 'Game Over'
+            isGameOver = true;
+        } 
 
-        let timerId = setInterval(function () {
-            obstaclePosition -=10
-            obstacle.style.left = obstaclePosition + 'px'
-        },20)
+        obstaclePosition -=10
+        obstacle.style.left= obstaclePosition +'px'
 
+      },20)
+      if (!isGameOver) setTimeout(generateObstacles, randomTime)
     }
-    generateObstacles()
+     generateObstacles()
 })
 
    
