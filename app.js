bce2fbf64b12b897d9b3db324c0b3059550b0c93
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-02-04 22:32:47 -0300

    dino basic jump

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dino = document.querySelector('.dino')
+    let isJumping = false
+
+    function control(e) {
+        if (e.keyCode === 32) {
+            if (!isJumping){
+                isJumping = true
+                jump()
+            }
+            jump()
+        }
+    }
+    document.addEventListener('keyup', control)
+
+    function jump() {
+        let position = 0
+        let timerId = setInterval(function () {
+
+
+            if (position === 150){
+                clearInterval(timerId)
+                console.log('down')
+                let downTimerId = setInterval(function (){
+                    if (position === 0){
+                        clearInterval(downTimerId)
+                        isJumping = false
+                    }
+                    position -= 30
+                    dino.style.bottom = position + 'px'
+                }, 20)
+
+            }
+
+            console.log('up')
+            position += 30
+            dino, style.bottom = position + 'px'
+        }, 20)
+    }
+
+})
+
+   
